3da14d144d7747b8951900993644a7955fa4a7bf
2trututu2-netizen <2trututu2@gmail.com>
2026-06-10 19:53:41 +0700

    Update ArticleCard.tsx

--- welvers/components/cards/ArticleCard.tsx
+++ welvers/components/cards/ArticleCard.tsx
@@ -11,7 +11,7 @@ export function ArticleCard({
 
   return (
     <Link
-      href={href as any}
+      href={`/${locale}/${article.slug}`}
       className="group flex flex-col rounded-xl border border-border/60 bg-background/60 p-4 text-sm hover:border-border hover:bg-background transition-colors"
     >
       <h2 className="mb-2 font-medium leading-snug">{article.title}</h2>
